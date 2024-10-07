8e5d59ba65729c72009a8c82cf31c4d970eb40f2
HJkim0726 <bryn@HJ>
2024-10-07 17:30:05 +0900

    hmi.cpp changed for 60hz

--- src/launch/launch.sh
+++ src/launch/launch.sh
@@ -1,13 +1,18 @@
 #! /bin/bash
+
+SETUP_BASH_PATH="../../install/setup.bash"
+
 source /opt/ros/humble/setup.bash
-source /home/bryn/hyu_ws/install/setup.bash
+source $SETUP_BASH_PATH
+
+
 
 terminator -x bash -c "ros2 run turtlesim turtlesim_node" &
 sleep 1
-terminator -x bash -c "source /home/bryn/hyu_ws/install/setup.bash; ros2 run team2_package moving_turtle" &
+terminator -x bash -c "source $SETUP_BASH_PATH; ros2 run team2_package moving_turtle" &
 sleep 1
-terminator -x bash -c "source /home/bryn/hyu_ws/install/setup.bash; ros2 run team2_package color_changer" &
+terminator -x bash -c "source $SETUP_BASH_PATH; ros2 run team2_package color_changer" &
 sleep 1
-terminator -x bash -c "source /home/bryn/hyu_ws/install/setup.bash; ros2 run team2_package hmi" &
+terminator -x bash -c "source $SETUP_BASH_PATH; ros2 run team2_package hmi" &
 sleep 1
-terminator -x bash -c "rviz2 -d /home/bryn/hyu_ws/src/rviz/launch.rviz"
+terminator -x bash -c "rviz2"
